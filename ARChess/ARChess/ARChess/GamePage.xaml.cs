a68117290c7c8c2ef87d650e74192fcb32543022
Ethan McGee <bulletshot60@gmail.com>
2012-10-04 20:16:35 -0400

    new marker pattern, split board management and drawing into separate class

--- ARChess/ARChess/ARChess/GamePage.xaml.cs
+++ ARChess/ARChess/ARChess/GamePage.xaml.cs
@@ -25,8 +25,6 @@ namespace ARChess
         private GameTimer timer;
         private ContentManager content;
         private SpriteBatch spriteBatch;
-        private float aspectRatio;
-        private int spacing_between_squares = 40;
 
         //This is the secret sauce, this will render the Silverlight content
         private UIElementRenderer uiRenderer;
@@ -52,7 +50,6 @@ namespace ARChess
 
             // Set the sharing mode of the graphics device to turn on XNA rendering
             SharedGraphicsDeviceManager.Current.GraphicsDevice.SetSharingMode(true);
-            aspectRatio = (float)SharedGraphicsDeviceManager.Current.GraphicsDevice.Viewport.AspectRatio;
 
             //We need the Spritebatch to render the camera stream
             spriteBatch = new SpriteBatch(SharedGraphicsDeviceManager.Current.GraphicsDevice);
@@ -97,7 +94,7 @@ namespace ARChess
             // Initialize the Detector
             //This need to be done AFTER the camera is initialized
             arDetector = new GrayBufferMarkerDetector();
-            var marker = Marker.LoadFromResource("resources/Marker_SLAR_16x16segments_80width.pat", 16, 16, 80);
+            var marker = Marker.LoadFromResource("resources/marker.pat", 16, 16, 80);
             arDetector.Initialize(System.Convert.ToInt32(photoCamera.PreviewResolution.Width), System.Convert.ToInt32(photoCamera.PreviewResolution.Height), 1, 4000, marker);
             isInitialized = true;
         }
@@ -181,60 +178,8 @@ namespace ARChess
             spriteBatch.End();
 
             //Draw our model
-            DrawModel(SharedGraphicsDeviceManager.Current);
+            new ChessBoard().Draw(SharedGraphicsDeviceManager.Current, markerResult, content);
 
-        }
-
-        /// <summary>
-        /// Method the draws out model
-        /// </summary>
-        /// <param name="graphics"></param>
-        protected void DrawModel(SharedGraphicsDeviceManager graphics)
-        {
-            if (markerResult != null) //Only draw the model if the marker is found
-            {
-                SharedGraphicsDeviceManager.Current.GraphicsDevice.DepthStencilState = DepthStencilState.Default;
-                SharedGraphicsDeviceManager.Current.GraphicsDevice.BlendState = BlendState.Opaque;
-                SharedGraphicsDeviceManager.Current.GraphicsDevice.SamplerStates[0] = SamplerState.LinearWrap;
-
-                //Load out 3d Model
-                Model lightSquare = content.Load<Model>("light_cube");
-                Model darkSquare = content.Load<Model>("dark_cube");
-                Vector3 cameraPosition = new Vector3(0, 0, 150);
-
-                for (int i = 0; i < 8; ++i)
-                {
-                    for (int j = 0; j < 8; ++j)
-                    {
-                        //generate alternating squares
-                        Model myModel = (i % 2 == 0 ? (j % 2 == 0 ? darkSquare : lightSquare) : (j % 2 == 0 ? lightSquare : darkSquare));
-
-                        Microsoft.Xna.Framework.Matrix[] transforms = new Microsoft.Xna.Framework.Matrix[myModel.Bones.Count];
-                        myModel.CopyBoneTransformsTo(transforms);
-                        // Draw the model. A model can have multiple meshes, so loop.
-                        foreach (ModelMesh mesh in myModel.Meshes)
-                        {
-                            foreach (BasicEffect effect in mesh.Effects)
-                            {
-                                Vector3 modelPosition = new Vector3((i < 5 ? (4 - i) * -1 : i - 4) * spacing_between_squares, (j < 5 ? (4 - j) * -1 : j - 4) * spacing_between_squares, 0);
-
-                                effect.EnableDefaultLighting();
-                                effect.World = Microsoft.Xna.Framework.Matrix.CreateScale(0.1f) *
-                                    (transforms[mesh.ParentBone.Index]
-                                    * mesh.ParentBone.Transform
-                                    * Microsoft.Xna.Framework.Matrix.CreateTranslation(modelPosition)
-                                    * markerResult.Transformation.ToXnaMatrix()
-                                );
-
-                                effect.View = Microsoft.Xna.Framework.Matrix.CreateLookAt(cameraPosition, Vector3.Zero, Vector3.Up);
-                                effect.Projection = Microsoft.Xna.Framework.Matrix.CreatePerspectiveFieldOfView(Microsoft.Xna.Framework.MathHelper.ToRadians(45.0f), aspectRatio, 1.0f, 10000.0f);
-                            }
-                            // Draw the mesh, using the effects set above.
-                            mesh.Draw();
-                        }
-                    }
-                }
-            }
         }
     }
 }
