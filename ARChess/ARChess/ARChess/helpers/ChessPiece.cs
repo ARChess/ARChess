adb09044813a48ce1cbe3e2473ded78ee992181d
Jordan Gestring <jgestri@g.clemson.edu>
2012-10-14 22:29:34 -0400

    Starting work on game state and chess piece classes

---
+++ ARChess/ARChess/ARChess/helpers/ChessPiece.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using SLARToolKit;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace ARChess.helpers
+{
+    public class ChessPiece
+    {
+        public static const int PAWN   = 0;
+        public static const int ROOK   = 1;
+        public static const int KNIGHT = 2;
+        public static const int BISHOP = 3;
+        public static const int QUEEN  = 4;
+        public static const int KING   = 5;
+
+        private int mType;
+        private int mPlayer;
+        private Model mModel;
+        Vector2 mPosition;
+        //Position
+
+        public ChessPiece(ContentManager _content, int _type, Vector2 _position)
+        {
+            mType = _type;
+
+            String modelName = "";
+            switch(_type)
+            {
+                case PAWN :
+                    modelName = "pawn_model";
+                    break;
+                case ROOK :
+                    break;
+                case KNIGHT :
+                    break;
+                case BISHOP :
+                    break;
+                case QUEEN :
+                    break;
+                case KING :
+                    break;
+            }
+            //mModel = _content.Load<Model>(modelName);
+            mPosition = _position;
+        }
+
+        public void Draw() {
+
+        }
+
+        public String toString()
+        {
+            String pieceString = "";
+
+            return pieceString;
+        }
+
+    }
+}
---
+++ ARChess/ARChess/ARChess/helpers/GameState.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Collections.Generic;
+using SLARToolKit;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+
+namespace ARChess.helpers
+{
+    public class GameState
+    {
+        private int scale = 31;
+        private ContentManager content;
+        private List<ChessPiece> mChessPieces;
+
+        public GameState()
+        {
+            Vector2 position;
+
+            position = new Vector2(0,1);
+            
+            // Initialize all ChessPieces for ech player
+            // Start with White team
+            mChessPieces.Add(new ChessPiece(content, ChessPiece.PAWN, new Vector2(0, 1)));
+        }
+
+        public GameState(String _stateString)
+        {
+            
+        }
+
+        private void initializePlayerPieces(int _player)
+        {
+            Vector2 position;
+            // Initialize Pawns
+            for (int i = 0; i < 8; i++)
+            {
+                position = new Vector2(i, 1);
+                mChessPieces.Add(new ChessPiece(content, ChessPiece.PAWN, position));
+            }
+
+            // Initialize Rooks
+            for (int i = 0; i < 2; i++)
+            {
+                position = new Vector2(i * 7, 0);
+                mChessPieces.Add(new ChessPiece(content, ChessPiece.PAWN, position));
+            }
+
+            // Initialize Knights
+            for (int i = 0; i < 2; i++)
+            {
+                position = new Vector2(i * 8, 0);
+                mChessPieces.Add(new ChessPiece(content, ChessPiece.PAWN, position));
+            }
+            
+        }
+
+        public void loadState(String _stateString)
+        {
+
+        }
+
+        public void Draw()
+        {
+
+        }
+
+        public String toString()
+        {
+            String stateString = "";
+            //stateString = mChessPieces.ToString();
+            return stateString;
+        }
+
+    }
+}
