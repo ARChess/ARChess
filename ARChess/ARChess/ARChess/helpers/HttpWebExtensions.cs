0a3fd79b0d4e113c5930dd214df89d15f8e7b247
Ethan McGee <bulletshot60@gmail.com>
2012-11-06 17:23:43 -0500

    add extensions to system.web, write networktask to use json
    api calls should now "work", I need to import the api key, at this point
    you are just getting an unauthorized exception that is thrown from the
    server, lovely :)

---
+++ ARChess/ARChess/ARChess/helpers/HttpWebExtensions.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace System.Net
+{
+    public static class HttpWebRequestExtensions
+    {
+        private const int DefaultRequestTimeout = 5000;
+
+        public static HttpWebResponse GetResponse(this HttpWebRequest request)
+        {
+            var dataReady = new AutoResetEvent(false);
+            HttpWebResponse response = null;
+            var callback = new AsyncCallback(delegate(IAsyncResult asynchronousResult)
+            {
+                response = (HttpWebResponse)request.EndGetResponse(asynchronousResult);
+                dataReady.Set();
+            });
+
+            request.BeginGetResponse(callback, request);
+
+            if (dataReady.WaitOne(DefaultRequestTimeout))
+            {
+                return response;
+            }
+
+            return null;
+        }
+
+        public static Stream GetRequestStream(this HttpWebRequest request)
+        {
+            var dataReady = new AutoResetEvent(false);
+            Stream stream = null;
+            var callback = new AsyncCallback(delegate(IAsyncResult asynchronousResult)
+            {
+                stream = (Stream)request.EndGetRequestStream(asynchronousResult);
+                dataReady.Set();
+            });
+
+            request.BeginGetRequestStream(callback, request);
+            if (!dataReady.WaitOne(DefaultRequestTimeout))
+            {
+                return null;
+            }
+
+            return stream;
+        }
+    }
+}
--- ARChess/ARChess/ARChess/helpers/NetworkTask.cs
+++ ARChess/ARChess/ARChess/helpers/NetworkTask.cs
@@ -2,6 +2,8 @@
 using System.IO;
 using System.Net;
 using System.Text;
+using System.Runtime.Serialization.Json;
+using System.ServiceModel;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
@@ -10,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
+using Microsoft.Phone.Info;
 
 namespace ARChess
 {
@@ -20,23 +23,68 @@ namespace ARChess
             
         }
 
-        private void makeHttpRequest(string path, string verb, string data)
+        private string getUniqueDeviceId()
         {
+            byte[] result = null;
+            object uniqueId;
+            if (DeviceExtendedProperties.TryGetValue("DeviceUniqueId", out uniqueId))
+                result = (byte[])uniqueId;
 
+            return Convert.ToBase64String(result);  
         }
 
-        public GameState getGameState()
+        private string makeHttpRequest(string verb, string data)
         {
-            return null;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://synchro-archess.herokuapp.com/api");
+            request.Method = verb;
+            request.ContentType = "application/json";
+            request.Headers["API_KEY"] = "";
+  
+            byte[] post_bytes = Encoding.UTF8.GetBytes(data);
+            Stream writer = request.GetRequestStream();
+            writer.Write(post_bytes, 0, post_bytes.Length);
+            writer.Close();
+  
+            HttpWebResponse response = request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            String returninfo = reader.ReadToEnd();
+            reader.Close();
+            response.Close();
+
+            return returninfo;
+        }
+
+        public GameResponse getGameState()
+        {
+            string response = makeHttpRequest("GET", "");
+
+            GameResponse state = null;
+
+            DataContractJsonSerializer js = new DataContractJsonSerializer(typeof(GameResponse));
+            MemoryStream stream = new MemoryStream(Encoding.UTF8.GetBytes(response));
+            state = (GameResponse)js.ReadObject(stream);
+
+            return state;
         }
 
         public bool sendGameState(GameState state)
         {
+            DataContractJsonSerializer js = new DataContractJsonSerializer(typeof(GameState));
+            MemoryStream stream = new MemoryStream();
+            js.WriteObject(stream, state);
+            stream.Position = 0;
+            StreamReader reader = new StreamReader(stream);
+            string json = reader.ReadToEnd();
+
+            makeHttpRequest("PUT", json);
+
             return true;
         }
 
         public bool resignGame()
         {
+            makeHttpRequest("DELETE", "");
+
             return true;
         }
     }
